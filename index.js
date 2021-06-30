c83aa3739c6e41c1c8729d9515c716fdec18bc15
AvezzaniNicolas <nicolas.avezzanirc@gmail.com>
2021-06-30 17:46:42 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+addEventListener("load", load);
+//llamo al servidor
+var servi = "http://localhost:444/login";
+
+function $(demo){
+    return document.getElementById(demo);
+}
+
+
+
+function load(){
+    document.getElementById("ingresar").addEventListener("click", click);
+}
+
+function click(){
+   enviarMensajeAlServidorPost(servi,retornoDelClick);
+
+}
+
+function retornoDelClick(respuesta){
+    if(respuesta == 'Bienvenido'){
+        location.replace("pantallaprincipal.html");
+    }else{
+        alert('error al ingresar');
+    }
+}
+
+function enviarMensajeAlServidor(servi, funcionARealizar){
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.open("GET",servi,true);
+
+    xmlhttp.onreadystatechange = function(){
+
+        if(xmlhttp.readyState == XMLHttpRequest.DONE){
+            if(xmlhttp.status == 200){
+                console.log(xmlhttp.response);
+                funcionARealizar(xmlhttp.responseText);
+            }else{
+                alert("Ocurrio un error");
+            }
+        }
+
+    }
+    xmlhttp.send();    
+}
+
+
+function enviarMensajeAlServidorPost(servidor, funcionARealizar) {
+
+    //declaro el objeto
+    var xmlhttp = new XMLHttpRequest();
+    var datos = new FormData();
+    datos.append("nombre",$("usuario").value);
+    datos.append("contrasena",$("contrasenia").value);
+
+    // indico hacia donde va el mensaje
+    xmlhttp.open("POST", servidor, true);
+    //seteo el evento
+    xmlhttp.onreadystatechange = function () {
+        //Veo si llego la respuesta del servidor
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+            //Reviso si la respuesta es correcta
+            if (xmlhttp.status == 200) {
+                funcionARealizar(xmlhttp.responseText);
+            }
+            else {
+                alert("ocurrio un error");
+            }
+        }
+    }
+
+   
+    xmlhttp.setRequestHeader("enctype", "multipart/form-data");
+
+    //envio el mensaje    
+    xmlhttp.send(datos);
+}
+
+
+
+/*function cargarOpcionesPeliculas(valor) {
+    var peliculas = JSON.parse(valor);
+    peliculas.sort(function (x, y) { return x.nombre.localeCompare(y.nombre) });
+    var opciones = ['<option value=0>Selecciones una pelicula</option>']
+
+    pelicula.forEach(element => {
+        opciones.push('<option value="' + element.valor + '">' + element.nombre + '</option>');
+    });
+
+    document.getElementById('peliculas').innerHTML = opciones;
+}*/
