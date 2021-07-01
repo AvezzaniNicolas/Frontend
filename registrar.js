e8b9459010d73c0b11b62f8bda771b8988059869
Nicolás avezzani <56846202+AvezzaniNicolas@users.noreply.github.com>
2021-07-01 17:40:54 -0300

    Update registrar.js

--- registrar.js
+++ registrar.js
@@ -49,7 +49,22 @@ function enviarMensajeAlServidorPost(servidor, funcionARealizar) {
     var datos = new FormData();
     datos.append("nombre",$("usuario1").value);
     datos.append("contrasena",$("contra1").value);
-
+    var usuario=document.getElementById('usuario1').value;
+    var contrasenia=document.getElementById('contra1').value;
+    var msg="llenar los siguientes campos que estan vacios:\n";
+    var ok=true;
+    if(usuario==""){
+        msg+="Usuario\n";
+        ok=false;
+    }
+    if(contrasenia==""){
+        msg+="contraseña\n";
+        ok=false;
+    }
+    if(ok==false){
+        alert(msg);
+    }else{
+        
     // indico hacia donde va el mensaje
     xmlhttp.open("POST", servidor, true);
     //seteo el evento
